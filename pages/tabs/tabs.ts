fe746116f60b227e391099a807e1ab6abaf1b72a
JayJay501 <j_crestin@yahoo.fr>
2021-06-22 23:32:09 +0200

    4 files modified, 2 added

--- pages/tabs/tabs.ts
+++ pages/tabs/tabs.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { PlayersPage } from '../players/players';
 import { HomePage } from '../home/home';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
   tab1Root = HomePage;
+  tab3Root = PlayersPage;
   tab2Root = AboutPage;
-  tab3Root = ContactPage;
 
-  constructor() {
-
-  }
+  constructor() {}
 }
